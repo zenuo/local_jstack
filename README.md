1c4efcb0031b9958324b23fb5969ab74c13ba51b
zenuo <zenuo@pm.me>
2025-03-30 11:10:15 +0800

    add test and doc for windows

--- README.md
+++ README.md
@@ -6,25 +6,38 @@ Thread dump like jstack in local JVM process (not interprocess like jstack in jd
 
 ## 支持的平台 Supported Platform
 
-- linux-x86_64
-- macos-x86_64
+- linux x64
+- macos x64
+- windows x64
 
 ## 上手 Get Started
 
 1.  首先找到JRE环境的`libjvm.so`中的[thread_dump(AttachOperation*, outputStream*)](https://github.com/openjdk/jdk/blob/742e735d7f6c4ee9ca5a4d290c59d7d6ec1f7635/src/hotspot/share/services/attachListener.cpp#L209)函数的偏移量
 
-    以21.0.5-ms为例，为`4892880`：
+    - unix平台，以21.0.5-ms为例，为`4892880`：
 
-    ```cmd
-    $ nm -t d -C /usr/local/sdkman/candidates/java/21.0.5-ms/lib/server/libjvm.so |grep -F 'thread_dump(AttachOperation*, outputStream*)'
-    0000000004892880 t thread_dump(AttachOperation*, outputStream*)
-    ```
+        ```cmd
+        $ nm -t d -C /usr/local/sdkman/candidates/java/21.0.5-ms/lib/server/libjvm.so |grep -F 'thread_dump(AttachOperation*, outputStream*)'
+        0000000004892880 t thread_dump(AttachOperation*, outputStream*)
+        ```
 
-    若是Windows平台，则需要借助map文件来获取函数偏移量，例如[microsoft-jdk-21.0.3-windows-x64.zip](https://aka.ms/download-jdk/microsoft-jdk-21.0.3-windows-x64.zip)，需要下载微软提供的调试信息[microsoft-jdk-debugsymbols-21.0.3-windows-x64.zip](https://aka.ms/download-jdk/microsoft-jdk-debugsymbols-21.0.3-windows-x64.zip)，然后解压调试信息
+    - Windows平台，则需要借助map文件来获取函数偏移量，例如[microsoft-jdk-21.0.3-windows-x64.zip](https://aka.ms/download-jdk/microsoft-jdk-21.0.3-windows-x64.zip)，需要下载微软提供的调试信息[microsoft-jdk-debugsymbols-21.0.3-windows-x64.zip](https://aka.ms/download-jdk/microsoft-jdk-debugsymbols-21.0.3-windows-x64.zip)，然后解压调试信息
 
-    ```cmd
-    findstr "?thread_dump@@YAJPEAVAttachOperation@@PEAVoutputStream@@@Z" .\server\jvm.dll.map | findstr /V unwind
-    ```
+        首先是获取`ImageBase`，如下命令的结果第三列`0000000180000000`就是`ImageBase`的Rva+Base 16进制形式：
+
+        ```
+        findstr "__ImageBase" C:\jdk-21.0.3+9-debug-symbols\bin\server\jvm.dll.map
+         0000:00000000       __ImageBase                0000000180000000     <linker-defined>
+        ```
+
+        再是获取`thread_dump(AttachOperation*, outputStream*)`方法在dll中的地址，如下命令的结果第三列`00000001801315e0`就是`thread_dump`函数Rva+Base16进制形式：
+
+        ```cmd
+        findstr "?thread_dump@@YAJPEAVAttachOperation@@PEAVoutputStream@@@Z" C:\jdk-21.0.3+9-debug-symbols\bin\server\jvm.dll.map | findstr /V unwind
+         0001:001305e0       ?thread_dump@@YAJPEAVAttachOperation@@PEAVoutputStream@@@Z 00000001801315e0 f   attachListener.obj
+        ```
+
+        然后两者做减法：`00000001801315e0 - 0000000180000000`的结果`1315e0`就是`thread_dump`函数在`jvm.dll`中的偏移量
 
 2. 使用java.io.Writer来承接线程栈
     ```java
--- script/testThreadDump.ps1
+++ script/testThreadDump.ps1
@@ -1,41 +1,28 @@
-﻿param(
-[string[]]$jdkVersions,
-[string]$jarPath,
-+[string]$dllPath,
-[bool]$download=$false
+﻿#.\testThreadDump.ps1 -jdkPath C:\download -jdkVersions 11.0.26, 21.0.3 -jarPath C:\download\localjstack-0.0.1.jar -dllPath C:\download\local_jstack\build\x86_windows_msvc2022_pe_32bit-Debug\src\main\cpp\localjstack.dll
+
+param(
+[Parameter(Mandatory=$true)][string[]]$jdkVersions,
+[Parameter(Mandatory=$true)][string]$jarPath,
+[Parameter(Mandatory=$true)][string]$dllPath,
+[bool]$download=$false,
+[Parameter(Mandatory=$true)][string]$jdkPath
 )
 
 $jarPath = resolve-path $jarPath
 $dllPath = [System.IO.Path]::GetDirectoryName($(Resolve-Path $dllPath))
 
-function Get-Address-ThreadDump{
-    param(
-    [string]$debugSymbolsPath
-    )
+Write-Host $dllPath
 
-    $jvmDllMap = "jvm.dll.map"
+. ($PSScriptRoot + "\Get-Offset-ThreadDump.ps1")
 
-    $foundFiles = Get-ChildItem -Path $debugSymbolsPath -Recurse -Filter $jvmDllMap
-
-    if ($null -eq $foundFiles) {
-        Write-Host "file not found: $jvmDllMap in $debugSymbolsPath"
-        exit 1
-    }
-
-    $jvmDllMapPath = $foundFiles[0]
-    Write-Host "jvmDllMap found: "$jvmDllMapPath.FullName
-
-    $threadDumpAddressLine = $(Select-String -SimpleMatch "?thread_dump@@YAJPEAVAttachOperation@@PEAVoutputStream@@@Z" -Path $jvmDllMapPath | Where-Object { $_ -notmatch "unwind" }).Line
-
-    return $(-split $threadDumpAddressLine)[2]
-}
+cd $jdkPath
 
 foreach ($i in $jdkVersions)  
 { 
     $jdkUrl = "https://aka.ms/download-jdk/microsoft-jdk-$i-windows-x64.zip"
     $debugSymbolsUrl = "https://aka.ms/download-jdk/microsoft-jdk-debugsymbols-$i-windows-x64.zip"
 
-    Write-Host "version: $i"
+    Write-Host "jdkVersion: $i"
 
     mkdir -Force $i
     cd $i
@@ -59,16 +46,21 @@ foreach ($i in $jdkVersions)
         Invoke-WebRequest $debugSymbolsUrl -OutFile debugsymbols.zip
         7z x debugsymbols.zip -odebugsymbols
     }
-    $threadDumpAddress = Get-Address-ThreadDump -debugSymbolsPath debugsymbols
-    Write-Host "threadDumpAddress found: $threadDumpAddress"
-    Write-Host $javaExe.FullName" -Dlocaljstack.threadDumpOffset=$threadDumpAddress -cp $jarPath app.PrintStack"
+    $threadDumpOffset = Get-Offset-ThreadDump -debugSymbolsPath debugsymbols -jdkVersion $i
+    Write-Host "threadDumpOffset found: $threadDumpOffset"
 
-    $command = "-Dlocaljstack.threadDumpOffset=$threadDumpAddress -Djava.library.path=$dllPath -cp $jarPath app.PrintStack"
-
+    $command = "`"-Dlocaljstack.threadDumpOffset=$threadDumpOffset`" `"-Djava.library.path=$dllPath`" -cp $jarPath app.PrintStack"
     Write-Host $javaExe.FullName $command
+	
+	$proc = Start-Process -FilePath $javaExe.FullName -ArgumentList $command -PassThru -Wait
+
     if ($proc.ExitCode -ne 0) {
         Write-Warning "$i exit with status code $($proc.ExitCode)"
+		exit $proc.ExitCode
     }
+	
+	Write-Host "jdkVersion passed: $i"
+
     cd ..
 }
 
